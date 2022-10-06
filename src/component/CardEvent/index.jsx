64faac9a7f7c78a60448198a4d523a306d0d5a00
graciasvito <vitoristo1@gmail.com>
2022-10-07 02:47:35 +0700

    delete console

--- src/component/CardEvent/index.jsx
+++ src/component/CardEvent/index.jsx
@@ -7,7 +7,6 @@ import moment from "moment/moment";
 // import eventA from "../../assets/image/Event A.png";
 
 function CardEvent(props) {
-  console.log(props);
   const navigate = useNavigate();
 
   const handleDetail = () => {
